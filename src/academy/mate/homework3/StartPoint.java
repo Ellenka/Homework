fa8da1720ad253074d7a588eb3b45326e661d345
Olena <olena.dobrovinska@gmail.com>
2018-12-24 17:37:01 +0200

    All changed executed, with running dog

--- src/academy/mate/homework3/StartPoint.java
+++ src/academy/mate/homework3/StartPoint.java
@@ -2,9 +2,9 @@ package academy.mate.homework3;
 
 public class StartPoint {
 public static void main(String args[]) {
-  /*LessonThreeHomework hw = new LessonThreeHomework();
+ LessonThreeHomework hw = new LessonThreeHomework();
   hw.first();
-  hw.cased();*/
+  hw.cased();
   
   WalkingDog walking = new WalkingDog();
   walking.doubleSymbylMoved();
