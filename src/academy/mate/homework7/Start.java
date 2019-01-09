aeb76e18c577b6cf0a133609e0e73d111679126d
Olena <olena.dobrovinska@gmail.com>
2019-01-09 20:15:31 +0200

    corrected mistake

--- src/academy/mate/homework7/Start.java
+++ src/academy/mate/homework7/Start.java
@@ -4,7 +4,12 @@ public class Start {
   public static void main(String[] args) {
     User user = new User();
     user.started();
-    
+    /*В классе User в каждом свиче повторяется 
+       shape.getInfo();
+       figures[i] = shape;
+       может, его можно вынести куда в одно место?
+     */
+     
     
   }
 
